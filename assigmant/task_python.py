fd5dc5e54faa5219c3544bfe93304529a9a7d06e
GioGio2004 <116484981+GioGio2004@users.noreply.github.com>
2024-01-27 20:10:12 +0400

    task_python.py

--- assigmant/task_python.py
+++ assigmant/task_python.py
@@ -2,8 +2,8 @@ import pandas as pd
 import time
 
 start_time = time.time()
-db = pd.read_csv('steam-200k.csv')
 
+db = pd.read_csv('steam-200k.csv')
 
 unique_cols = ['151603712', 'The Elder Scrolls V Skyrim', 'purchase', '1.0', '0']
 
@@ -14,13 +14,17 @@ dup_data.to_csv('steam-200k-new.csv', index=False)
 # Measuring the execution time from start to end 
 end_time = time.time()
 total_time = end_time - start_time
-print(f"Execution time: {total_time:.2f} seconds")
 
 output_data = pd.read_csv('steam-200k-new.csv')
+ 
+before_deduplication = len(db)
+after_deduplication = len(output_data)
 
+# number of deduplicates
 number_of_duplicates = len(db) - len(output_data)
+
+# showing results
+print(f"execution time: {total_time:.2f} seconds")
+print(f"number of records in dataset befor deduplication {before_deduplication}")
+print(f"number of records in dataset after deduplication {after_deduplication}")
 print(f"Number of duplicates: {number_of_duplicates}")
-
-new_db = pd.read_csv('steam-200k-new.csv')
-
-print(new_db)
